9b609c7c050cbe847f62762900e2469f3e2707d7
akariiff <akarisecaccound@gmail.com>
2025-07-29 16:20:09 +0700

    Add files via upload

---
+++ valentine-confession.js
@@ -0,0 +1,47 @@
+
+let currentSlide = 0;
+const slideEl = document.getElementById('slides');
+const bgm = document.getElementById('bgm');
+
+function updateSlide() {
+  slideEl.style.transform = `translateX(-${currentSlide * 100}vw)`;
+
+  if (currentSlide === 1) {
+    bgm.pause();
+  } else {
+    bgm.play().catch(() => {});
+  }
+
+  if (currentSlide === 2) {
+    $('#flowersContent').load('https://akariiff.github.io/flowers-for-her/');
+  }
+}
+
+function nextSlide() {
+  if (currentSlide < 2) {
+    currentSlide++;
+    updateSlide();
+  }
+}
+
+function prevSlide() {
+  if (currentSlide > 0) {
+    currentSlide--;
+    updateSlide();
+  }
+}
+
+$(document).ready(function () {
+  $('#messageState').change(function () {
+    const msg = $('.message'), heart = $('.heart');
+
+    if (this.checked) {
+      bgm.play().catch(() => {});
+      msg.removeClass('closeNor').addClass('openNor');
+      heart.removeClass('closeHer').addClass('openHer');
+    } else {
+      msg.removeClass('openNor').addClass('closeNor');
+      heart.removeClass('openHer').addClass('closeHer');
+    }
+  });
+});
